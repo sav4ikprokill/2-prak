feca041deb9ebaec47f1868c73752e90143c4111
Cybersport <Cybersport@26kadr.internal>
2025-02-28 14:50:25 +0300

    верстка титле

--- components/shader/title.tsx
+++ components/shader/title.tsx
@@ -2,4 +2,34 @@ import clsx from 'clsx';
 import React from 'react';
 
 type TitleSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl';
-add
+
+interface Props {
+    size?: TitleSize;
+    className?: string;
+    text: string;
+}
+
+export const Title: React.FC<Props> = ({ text, size = 'sm', className }) => {
+    const mapTagSize = {
+        xs: 'h5',
+        sm: 'h4',
+        md: 'h3',
+        lg: 'h2',
+        xl: 'h1',
+        '2xl': 'h1',
+    } as const;
+    const mapClassNameBySize = {
+        xs: 'text-[16px]',
+        sm: 'text-[22px]',
+        md: 'text-[26px]',
+        lg: 'text-[32px]',
+        xl: 'text-[40px]',
+        '2xl': 'text-[48px]',
+    } as const;
+   
+    return React.createElement(
+        mapTagSize[size],
+        { className: clsx(mapClassNameBySize[size], className) },
+        text,
+    );
+};
